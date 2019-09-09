49f8c370acd2b1ad2d7ed64783fc13a2f47b01e1
Jun Kimura <junkxdev@gmail.com>
2019-09-09 17:09:59 +0900

    fix deprecated syntax

--- hmcdk/lib/src/utils.rs
+++ hmcdk/lib/src/utils.rs
@@ -7,9 +7,9 @@ pub fn hex_to_bytes(hex_asm: &str) -> Vec<u8> {
     let mut hex_bytes = bs
         .iter()
         .filter_map(|b| match b {
-            b'0'...b'9' => Some(b - b'0'),
-            b'a'...b'f' => Some(b - b'a' + 10),
-            b'A'...b'F' => Some(b - b'A' + 10),
+            b'0'..=b'9' => Some(b - b'0'),
+            b'a'..=b'f' => Some(b - b'a' + 10),
+            b'A'..=b'F' => Some(b - b'A' + 10),
             _ => None,
         })
         .fuse();
